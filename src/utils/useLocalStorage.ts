1a2f28ead72052996988f595ede3cda6fb0ab7ba
Park Gyu Ae <yunos0502@gmail.com>
2021-06-01 03:58:09 +0900

    refactor: localStorage 타입스크립트로 변경 및 custom useHooks

---
+++ src/utils/useLocalStorage.ts
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+export default function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, (value: T | ((val: T) => T)) => void] {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const getStorage: any = localStorage.getItem(key);
+      return getStorage ? JSON.parse(getStorage) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T | ((val: T) => T)) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
