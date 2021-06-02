cde13a6452465ed9673274ae8cefe647ec90b91b
Park Gyu Ae <yunos0502@gmail.com>
2021-06-02 22:26:29 +0900

    feat: Add LinkItem.tsx

---
+++ src/components/LinkItem.tsx
@@ -0,0 +1,30 @@
+import React, { MouseEventHandler } from 'react';
+import { CopyLinkButton, DeleteLinkButton } from './Buttons';
+
+interface ItemProps {
+  link: {
+    id: string,
+    title: string,
+    link: string
+  }, 
+  copyLinkHandler: MouseEventHandler<HTMLButtonElement>, 
+  onDeleteLink: MouseEventHandler<HTMLButtonElement>
+}
+
+const LinkItem = ({ link, copyLinkHandler, onDeleteLink }: ItemProps) => {
+  const onUrlCopyHandler = (e: any): void => {
+    e.preventDefault();
+    copyLinkHandler(e.target.previousSibling.innerText);
+  };
+
+  return (
+    <li key={link.id} data-_id={link.id}>
+      <span>{link.title}</span>
+      <span>{link.link}</span>
+      <CopyLinkButton onClick={onUrlCopyHandler} />
+      <DeleteLinkButton onClick={onDeleteLink} />
+    </li>
+  );
+};
+
+export default LinkItem;
